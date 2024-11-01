9a7f49e05181f46912b6f4be226c09834c7157ac
manoj hc <manojhc.dev@gmail.com>
2024-11-01 16:19:58 +0530

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -1,7 +1,7 @@
 print("hello world)
 #priting user name using f string
-name = input("Enter Your Name :" )
-print(f"Your Name is {name}")
+name_using_f_func = input("Enter Your Name :" )
+print(f"Your Name is {name_using_f_func}")
 
 #replacing the name using replace fucntion
 
