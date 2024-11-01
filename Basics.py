ddcfea2a819ceb640cb7d61472b9da481096f01b
manoj hc <manojhc.dev@gmail.com>
2024-11-01 15:59:41 +0530

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -2,3 +2,7 @@ print("hello world)
 #priting user name using f string
 name = input("Enter Your Name :" )
 print(f"Your Name is {name}")
+
+#replacing the name using replace fucntion
+example = '''Dear Name ,You are selected!,Date'''
+print(example.replace("Dear Name", "Manu").replace("Date", "12/1/2023"))
