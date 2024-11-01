058b896d4813dd6e563ad4f4f0078dc86fc151a7
manoj hc <manojhc.dev@gmail.com>
2024-11-01 16:05:16 +0530

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -6,3 +6,7 @@ print(f"Your Name is {name}")
 #replacing the name using replace fucntion
 example = '''Dear Name ,You are selected!,Date'''
 print(example.replace("Dear Name", "Manu").replace("Date", "12/1/2023"))
+
+#Finding a letter s and double space using "fing" func
+name = "HELLO BRO THIS IS  PYTHON"
+print(name.find("  "))
