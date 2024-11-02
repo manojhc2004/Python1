4a214feb2bcfe745ae72c1e489d8e29f5bcef933
manoj hc <manojhc.dev@gmail.com>
2024-11-02 21:13:49 +0530

    Update Tasks.py

--- Tasks.py
+++ Tasks.py
@@ -157,3 +157,12 @@ else:
 user = int(input("Enter a number :"))
 for i in range(1,11):
   print(f"{i} X {i} = {user*i}")
+
+#Finding a number is prime or not
+n = int(input("Enter number"))
+for i in range(2,n):
+  if(n%i==0):
+    print("The number is not prime")
+    break
+else:
+    print("The number is prime")
