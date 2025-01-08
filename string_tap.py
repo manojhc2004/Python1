f265a746a6660bbe80df2056c62ec83df2199593
manoj hc <manojhc.dev@gmail.com>
2025-01-09 01:18:54 +0530

    Update string_tap.py

--- string_tap.py
+++ string_tap.py
@@ -5,3 +5,11 @@ print(name)
 #string slicing
 s = "guido van rossum"
 print(s[0:6:2]) #[start:end:step]
+
+#reverse slicing
+s = "guido van rossum"
+print(s[::-1]) #reversing a string
+
+#negative slicing
+s = "guido van rossum" 
+print(s[-1:0:-1]) #negative slicing  -1 will change the direction
