c473e8e19c84caec3d820ab4fc7031e8bd8c1879
manoj hc <manojhc.dev@gmail.com>
2025-01-09 01:00:30 +0530

    Update string_tap.py

--- string_tap.py
+++ string_tap.py
@@ -1,3 +1,7 @@
 #string is immutiable it can't change anything
 name = "practice make man 'perfect'" #To highlighting a string or letter
 print(name)
+
+#string slicing
+s = "guido van rossum"
+print(s[0:6:2]) #[start:end:step]
