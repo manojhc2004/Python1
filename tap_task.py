f4f063905da4a1eac3d525192737d760ff74c35a
manoj hc <manojhc.dev@gmail.com>
2025-01-09 23:48:06 +0530

    Update tap_task.py

--- tap_task.py
+++ tap_task.py
@@ -2,3 +2,11 @@
 user = input("enter a string:")
 for i in range(0,len(user)-2):
     print(user[i:i+3])
+
+#printing the string ignoring the first character ans last character
+user = input("enter a string:")
+for i in range(1,len(user)-1):
+  print(user[i])
+    #or
+ans = user[1:-1]
+print(ans)
