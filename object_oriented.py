78eaf5da70e8549d67bfa992a093137a8dfa2906
manoj hc <manojhc.dev@gmail.com>
2025-02-16 15:46:28 +0530

    Update object_oriented.py

--- object_oriented.py
+++ object_oriented.py
@@ -9,3 +9,26 @@ class human:
 
 ninja = human("ninja",20)
 ninja.running()
+
+class caluclator:
+    def __init__(self,num1,num2):
+        self.num1 = num1
+        self.num2 = num2
+    def addition(self):
+        res = self.num1 + self.num2
+        print(f"the addtion is {res}")
+    def subraction(self):
+        res = self.num1 - self.num2
+        print(f"the subraction is {res}")
+    def multiflication(self):
+        res = self.num1 * self.num2
+        print(f"the multiflication is {res}")
+    def divition(self):
+        res = self.num1 / self.num2
+        print(f"the divition is {res}")
+
+num = caluclator(5 , 5)
+num.addition()
+num.subraction()
+num.multiflication()
+num.divition()
